6cea6bc4a01d5a5b64109d2668283a3c76558e82
ll06-P <ll06printf@outlook.com>
2025-03-17 13:21:26 +0800

    更改 Transformable 实现
    - [add] Transformable 现在可以用于拥有多个分量的元组变体。

--- lib/visitor_template/src/lib.rs
+++ lib/visitor_template/src/lib.rs
@@ -4,11 +4,11 @@ use core::panic;
 
 use crate::proc_macro::TokenStream;
 
-use syn::{parse_macro_input, DeriveInput, Fields, FieldsUnnamed, Type, TypePath};
+use syn::{parse_macro_input, DeriveInput, Fields, FieldsUnnamed, Type};
 
 struct VariantPair {
     ident: syn::Ident,
-    structure: TypePath,
+    params: Vec<Type>,
 }
 
 
@@ -32,42 +32,35 @@ pub fn transformable_derive(item: TokenStream) -> TokenStream {
 fn get_enum_variant_names(t: DeriveInput) -> Vec<VariantPair> {
     use syn::Data::Enum;
 
-    let enum_id = t.ident;
     let mut ans = Vec::new();
 
     if let Enum(enum_data) = t.data {
         for variant in enum_data.variants {
 
             let variant_name = variant.ident;
+            let mut variant_member = Vec::new();
 
             // 解析元组
             // 这样写似乎没有增加可读性。
             if let Fields::Unnamed(FieldsUnnamed{paren_token:_, unnamed:members}) = variant.fields {
-                let sct_ty =  if members.len() > 1 {
-                    panic!("Every enum({}) tuple-like field({}) are allowed to have exactly one member", enum_id, variant_name);
-                } else {
-                    let sct_ty = members[0].ty.clone();
-                    match sct_ty {
-                        Type::Path(ty_path) => ty_path,
-                        _ => panic!("Only type path allowed in enum field"),
-                    }
-                };
-
-                ans.push(VariantPair{
-                    ident: variant_name,
-                    structure: sct_ty,
-                })
-
+                for member in members {
+                    variant_member.push(member.ty.clone());
+                }
             } else {
-                panic!("There is enum field {} that isn't a tuple-like", variant_name);
+                panic!("There is enum field {} that isn't a tuple-like variant", variant_name);
             }
+
+            ans.push(VariantPair{
+                ident: variant_name,
+                params: variant_member,
+            })
 
         }
 
     } else {
         panic!("Only enum accepted");
     }
-    return ans;
+    ans
 }
 
 // -- 代码生成 -- //
@@ -76,9 +69,10 @@ mod sysy_macro_gen {
 
     use proc_macro2::{TokenStream, Span};
 
-    use quote::quote;
+    use quote::{quote, ToTokens};
     use super::VariantPair;
 
+    /// 实现 Transform 和接收函数
     pub(crate) fn impl_accept_fn(ident: &syn::Ident, variants: &Vec<VariantPair>) -> TokenStream {
         let transformer_id = gen_transformer_id(ident);
         let accept_function = gen_accept_fn(variants);
@@ -91,12 +85,16 @@ mod sysy_macro_gen {
         }
     }
 
+    /// 生成接收函数的函数体
     fn gen_accept_fn(variants: &Vec<VariantPair>) -> TokenStream {
         let match_item = variants.iter().map(
-            |VariantPair{ident:id, structure:_}| {
+            |VariantPair{ident:id, params:p}| {
+
                 let fn_name = gen_transform_fn_name(id);
+                let (param_name, _) = gen_param_list(p);
+
                 quote! {
-                    Self::#id(v) => tr.#fn_name(v)
+                    Self::#id( #( #param_name),* ) => tr.#fn_name( #( #param_name ),* )
                 }
             }
         );
@@ -107,10 +105,12 @@ mod sysy_macro_gen {
         }
     }
 
+    /// 生成转换器 Trait
     pub(crate) fn impl_transformer(ident: &syn::Ident, variants: &Vec<VariantPair>) -> TokenStream {
         gen_transformer_trait(ident, variants)
     }
 
+    /// 生成转换器 Trait 的内容
     fn gen_transformer_trait(ident: &syn::Ident, variants: &Vec<VariantPair>) -> TokenStream {
         let transformer_name = gen_transformer_id(ident);
         let transform_functions= variants.iter().map(|v| gen_transform_fn(ident, v));
@@ -123,24 +123,26 @@ mod sysy_macro_gen {
         }
     }
 
+    /// 生成转换函数
     fn gen_transform_fn(enum_id: &syn::Ident, variant: &VariantPair) -> TokenStream {
         let fn_name = gen_transform_fn_name(&variant.ident);
         let visit_fn_name = gen_visit_fn_name(&variant.ident);
         let variant_name = variant.ident.clone();
-        let fn_param = variant.structure.clone();
+        let (fn_params , param_list)= gen_param_list(&variant.params);
         quote! {
-            fn #fn_name(&mut self, param: #fn_param) -> #enum_id {
-                self.#visit_fn_name(&param);
-                #enum_id::#variant_name(param)
+            fn #fn_name(&mut self, #( #param_list),* ) -> #enum_id {
+                self.#visit_fn_name( #( &#fn_params),* );
+                #enum_id::#variant_name( #( #fn_params ),* )
             }
         }
     }
 
+    /// 生成访问函数
     fn gen_visit_fn(variant: &VariantPair) -> TokenStream {
         let fn_name = gen_visit_fn_name(&variant.ident);
-        let fn_param = variant.structure.clone();
+        let (_, param_list) = gen_borrow_param_list(&variant.params);
         quote! {
-            fn #fn_name(&mut self, param: &#fn_param) -> () {
+            fn #fn_name(&mut self, #( #param_list),* ) -> () {
 
             }
         }
@@ -148,7 +150,7 @@ mod sysy_macro_gen {
 
 
 
-    // -- id 生成 -- //
+    // -- 记号生成 -- //
 
     /**
      * 生成一个 enum 对应的访问其的类型名
@@ -158,15 +160,56 @@ mod sysy_macro_gen {
         syn::Ident::new(&transformer_name, Span::call_site())
     }
 
+    /// 根据变体名字，生成转换函数的名字
     fn gen_transform_fn_name(variant_id: &syn::Ident) -> syn::Ident {
         let fn_name = String::from("transform_") + &*to_snake_case(&*variant_id.to_string());
         syn::Ident::new(&fn_name, Span::call_site())
     }
 
+    /// 根据变体的名字，生成访问函数的名字
     fn gen_visit_fn_name(variant_id: &syn::Ident) -> syn::Ident {
         let fn_name = String::from("visit_") + &*to_snake_case(&*variant_id.to_string());
         syn::Ident::new(&fn_name, Span::call_site())
     }
+
+    /// 对每一个变体调用转换函数的参数名，和形参列表。
+    fn gen_param_list(variant_param: &Vec<syn::Type>) -> (Vec<syn::Ident>, Vec<TokenStream>){
+        let (name, ty) = get_param_name_and_type(variant_param);
+        let param_list = name.iter().zip(ty.into_iter()).map(|(n, y)| {
+            quote! {
+                #n:#y
+            }
+        }).collect();
+
+        (name, param_list)
+    }
+
+    fn gen_borrow_param_list(variant_param: &Vec<syn::Type>) -> (Vec<syn::Ident>, Vec<TokenStream>){
+        let (name, ty) = get_param_name_and_type(variant_param);
+        let param_list = name.iter().zip(ty.into_iter()).map(|(n, y)| {
+            quote! {
+                #n:&#y
+            }
+        }).collect();
+
+        (name, param_list)
+    }
+
+    fn get_param_name_and_type(variant_param: &Vec<syn::Type>) -> (Vec::<syn::Ident>, Vec::<syn::Type>) {
+        let mut param_names = Vec::new();
+        let mut param_type = Vec::new();
+
+        for ty in variant_param.iter() {
+            let name = ty.to_token_stream().to_string();
+            let snake_name = syn::Ident::new(&to_snake_case(&name), Span::call_site());
+
+            param_names.push(snake_name.clone());
+            param_type.push(ty.clone());
+        }
+        (param_names, param_type)
+
+    }
+
     fn to_snake_case(s: &str) -> String {
         let mut snake = String::new();
         let mut prev_char = '_'; // 初始状态假设前一个字符是分隔符
